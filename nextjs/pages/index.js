97d1275bb710a938fc88f824c39c285b68acfe9e
Ayan Maiti <100613640+moonman369@users.noreply.github.com>
2023-01-22 15:26:47 +0530

    Add sitename and tab icon

--- nextjs/pages/index.js
+++ nextjs/pages/index.js
@@ -1,5 +1,5 @@
-import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import Head from "next/head";
 import Header from "../components/header/Header";
 import { ConnectButton } from "web3uikit";
 import LotteryEntrance from "../components/lottery/Lottery";
@@ -11,6 +11,10 @@ export default function Home() {
   const { isWeb3Enabled } = useMoralis();
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Ethereum Raffle DApp</title>
+        <link rel="icon" href="https://cdn-icons-png.flaticon.com/512/3531/3531226.png"/>
+      </Head>
       <nav className="p-5 border-b-2 flex flex-row">
         <div className={styles.header}>
           <Header />
