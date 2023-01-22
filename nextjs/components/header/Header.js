e1d7abf1e30c697b9a7652a12ab49ce48afd313f
Ayan Maiti <100613640+moonman369@users.noreply.github.com>
2023-01-22 15:40:05 +0530

    Change Header title

--- nextjs/components/header/Header.js
+++ nextjs/components/header/Header.js
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     // <nav className="p-5 border-b-2 flex flex-row">
     <div>
-      <h1 className="py-4 px-4 font-bold text-3xl">Decentralized Raffle</h1>
+      <h1 className="py-4 px-4 font-bold text-3xl">Ethereum Raffle</h1>
       <br />
       {/* <div className="ml-auto py-2 px-4">
         <ConnectButton moraLisAuth={false} />
