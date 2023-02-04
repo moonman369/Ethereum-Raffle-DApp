dd297346ce33bfeb571afee7ce5ac8ae52c8ab1d
Ayan Maiti <100613640+moonman369@users.noreply.github.com>
2023-02-04 12:15:35 +0530

    Add script for debugging purposes, debug.js Complete start-raffle.js install prompt package to take value from terminal

--- scripts/start-raffle.js
+++ scripts/start-raffle.js
@@ -1,53 +1,35 @@
 const { ethers } = require('hardhat');
-require('dotenv').config();
-const { abi } = require('../artifacts/contracts/raffle.sol/Raffle.json');
+const prompt = require("prompt");
+require("dotenv").config();
+const { abi } = require("../artifacts/contracts/raffle.sol/Raffle.json");
 
 const rpcUrl = process.env.GOERLI_RPC_URL;
 const raffleV2Address = process.env.RAFFLE_V2_ADDRESS;
+const ownerPvtKey = process.env.PRIVATE_KEY_G;
 const provider = new ethers.providers.JsonRpcProvider(rpcUrl);
 const raffle = new ethers.Contract(raffleV2Address, abi, provider);
 
-// 'http://127.0.0.1:8545'
-
-const sleep = async (ms) => {
-    setTimeout(() => {}, ms);
-}
-
 const startRaffle = async (duration) => {
-    const owner = new ethers.Wallet(process.env.PRIVATE_KEY_G, provider)
-
-    // const owner = await ethers.getSigners()
-
-    
-
-    const tx = await raffle.connect(owner).startRaffle(10).catch(error => console.error(error));
-    await tx.wait()
-
-
-    const tx2 = await raffle.connect(owner).enterRaffle({value: ethers.utils.parseEther('0.01')})
-    await tx2.wait()
-
-    console.log(await raffle.getRaffleState());
-
-}
-
-const getRaffleState = async () => {
-    const state = await raffle.getRaffleState()
-    console.log(state)
-}
+  const signer = new ethers.Wallet(ownerPvtKey, provider);
+  const tx = await raffle.connect(signer).startRaffle(duration);
+  const res = await tx.wait();
+  return res;
+};
 
 const main = async () => {
-    const tx1 = await getRaffleState();
-    
-    const tx2 = await startRaffle();
+  prompt.start();
 
-    const tx3 = sleep(15000);
+  prompt.get(["Duration"], async (error, result) => {
+    duration = result.Duration;
+    const txResult = await startRaffle(duration);
+    console.log(txResult);
+  });
+};
 
-    await getRaffleState();
-}
-
-main().catch((error) => {
-    console.error(error)
-}).then(() => {
-    process.exit(0);
-})
+main()
+  .catch((error) => {
+    console.error(error);
+  })
+  .then(() => {
+    // process.exit(0);
+  });
